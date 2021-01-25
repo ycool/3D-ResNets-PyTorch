ddb79ae3e65ebfa4c764d0486c87b115adcb43e0
Jiangtao Hu <boris.hu@gmail.com>
2021-01-25 15:12:32 -0800

    use baseline in script.

--- hmdb51_inference.sh
+++ hmdb51_inference.sh
@@ -2,6 +2,7 @@
 
 set -x 
 
+# baseline
 python main.py \
        --root_path datasets/hmdb51 \
        --video_path image \
@@ -18,9 +19,29 @@ python main.py \
        --output_topk 5 \
        --inference_batch_size 1 \
        --model_depth 50 \
-       --model resnext
+       --model resnet
 
-#       --model_depth 121 \
+
+# resnext 
+# python main.py \
+#        --root_path datasets/hmdb51 \
+#        --video_path image \
+#        --annotation_path hmdb51_test.json \
+#        --inference_subset test \
+#        --result_path results \
+#        --dataset hmdb51 \
+#        --resume_path results/save_200.pth \
+#        --n_classes 51 \
+#        --n_threads 4 \
+#        --no_train \
+#        --no_val \
+#        --inference \
+#        --output_topk 5 \
+#        --inference_batch_size 1 \
+#        --model_depth 50 \
+#        --model resnext
+
+#       --Model_depth 121 \
 #       --model densenet 
        
 # --model preresnet
--- hmdb51_train.sh
+++ hmdb51_train.sh
@@ -9,20 +9,35 @@ set -x
 # time python main.py --root_path datasets/hmdb51 \
 # time python -m pdb main.py --root_path datasets/hmdb51 \
 # time python -m memory_profiler main.py --root_path datasets/hmdb51 \
- time python main.py --root_path datasets/hmdb51 \
+
+# baseline
+time python main.py --root_path datasets/hmdb51 \
        --video_path image \
        --annotation_path hmdb51_total.json \
        --result_path results \
        --dataset hmdb51 \
-       --model resnext \
-       --resnext_cardinality 32 \
+       --model resnet \
        --model_depth 50 \
        --n_classes 51 \
        --batch_size 128 \
        --n_threads 4 \
-       --checkpoint 50 \
-       --tensorboard \
-       --n_epochs 200
+       --checkpoint 30
+
+# resnext
+# time python main.py --root_path datasets/hmdb51 \
+#        --video_path image \
+#        --annotation_path hmdb51_total.json \
+#        --result_path results \
+#        --dataset hmdb51 \
+#        --model resnext \
+#        --resnext_cardinality 32 \
+#        --model_depth 50 \
+#        --n_classes 51 \
+#        --batch_size 128 \
+#        --n_threads 4 \
+#        --checkpoint 50 \
+#        --tensorboard \
+#        --n_epochs 200
 
 #       --model resnet \
 #       --model_depth 50 \
